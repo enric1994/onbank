77b22c11830115dda735f406520df63f161af3e7
dazcona <david.azcona@asu.edu>
2019-06-22 12:36:02 +0100

    awesome

---
+++ src/static/js/example.js
@@ -0,0 +1,27 @@
+var lezkairu = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties":  {
+            'category': 'ATM',
+            'popupContent': '<strong> ATM </strong>',
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -3.334855, 55.934994 ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+            'category': 'branch',
+            'popupContent': '<strong> Branch </strong>',
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ -3.333825, 55.935033 ]
+        }
+      },
+  ]
+}
