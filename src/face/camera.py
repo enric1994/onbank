249b37f19140089d2c1194f6ade47f8479e11b2a
Enric Moreu <enric.moreu.filella@gmail.com>
2019-06-22 12:11:13 +0100

    Draw face landmark

--- src/face/camera.py
+++ src/face/camera.py
@@ -16,28 +16,46 @@ class VideoCamera(object):
     
     def get_frame(self):
         success, image = self.video.read()
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        small_image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+        small_image = small_image[:, :, ::-1]
 
-        small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+        face_landmarks_list = face_recognition.face_landmarks(small_image)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        for face_landmarks in face_landmarks_list:
+            color = (0,255,0)
+            radius = 5
+            thickness = 2
+            resize = 4
 
-        face_locations = face_recognition.face_locations(rgb_small_frame, model="hog")
-        
-        for top, right, bottom, left in face_locations:
-        
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+            cv2.circle(image, (face_landmarks['left_eyebrow'][0][0]*resize, face_landmarks['left_eyebrow'][0][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['left_eyebrow'][1][0]*resize, face_landmarks['left_eyebrow'][1][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['left_eyebrow'][2][0]*resize, face_landmarks['left_eyebrow'][2][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['left_eyebrow'][3][0]*resize, face_landmarks['left_eyebrow'][3][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['left_eyebrow'][4][0]*resize, face_landmarks['left_eyebrow'][4][1]*resize), radius, color,thickness)
 
-        #     # Draw a box around the face
-            cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.circle(image, (face_landmarks['right_eyebrow'][0][0]*resize, face_landmarks['right_eyebrow'][0][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['right_eyebrow'][1][0]*resize, face_landmarks['right_eyebrow'][1][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['right_eyebrow'][2][0]*resize, face_landmarks['right_eyebrow'][2][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['right_eyebrow'][3][0]*resize, face_landmarks['right_eyebrow'][3][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['right_eyebrow'][4][0]*resize, face_landmarks['right_eyebrow'][4][1]*resize), radius, color,thickness)
+
+            cv2.circle(image, (face_landmarks['nose_tip'][0][0]*resize, face_landmarks['nose_tip'][0][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['nose_tip'][1][0]*resize, face_landmarks['nose_tip'][1][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['nose_tip'][2][0]*resize, face_landmarks['nose_tip'][2][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['nose_tip'][3][0]*resize, face_landmarks['nose_tip'][3][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['nose_tip'][4][0]*resize, face_landmarks['nose_tip'][4][1]*resize), radius, color,thickness)
+
+            cv2.circle(image, (face_landmarks['top_lip'][0][0]*resize, face_landmarks['top_lip'][0][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['top_lip'][1][0]*resize, face_landmarks['top_lip'][1][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['top_lip'][5][0]*resize, face_landmarks['top_lip'][5][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['top_lip'][6][0]*resize, face_landmarks['top_lip'][6][1]*resize), radius, color,thickness)
 
 
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
+            cv2.circle(image, (face_landmarks['bottom_lip'][0][0]*resize, face_landmarks['bottom_lip'][0][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['bottom_lip'][1][0]*resize, face_landmarks['bottom_lip'][1][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['bottom_lip'][5][0]*resize, face_landmarks['bottom_lip'][5][1]*resize), radius, color,thickness)
+            cv2.circle(image, (face_landmarks['bottom_lip'][6][0]*resize, face_landmarks['bottom_lip'][6][1]*resize), radius, color,thickness)
+
+
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
